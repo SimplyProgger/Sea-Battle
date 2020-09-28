5d280cbf77da3f8ff34250f98ef1aa72a953ff58
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-09-28 10:45:52 +0500

    create player class

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 # Console Sea-Battle by SimplyProgger
 
-top_margin = [' ', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И' ,'К']
-player_1_field = [['~' for i in range(10)] for j in range(10)]
 
-for i in range(10):
-	list_pred = player_1_field[i]
-	new_list = [i] + list_pred
-	player_1_field[i] = new_list
-	
-pred_list = player_1_field
-player_1_field = [top_margin] + pred_list
 
-for i in player_1_field:
+
+class Player:
+
+	def __init__(self):
+
+		self.player_field = self.createField()
+
+
+
+	def createField(self):
+		top_margin = [' ', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И' ,'К']
+		player_field = [['~' for i in range(10)] for j in range(10)]
+
+		for i in range(10):
+			list_pred = player_field[i]
+			new_list = [i] + list_pred
+			player_field[i] = new_list
+			
+		pred_list = player_field
+		player_field = [top_margin] + pred_list
+
+		return player_field
+
+
+
+
+
+player1 = Player()
+for i in player1.player_field:
 	print(' '.join([str(j) for j in i]))
